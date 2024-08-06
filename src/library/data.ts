2c72d7a5788289b5b929c47b3c8a9a446553a0d4
jaironf <jaironf.94@gmail.com>
2024-08-06 13:25:40 +0200

    Añadido primer album

--- src/library/data.ts
+++ src/library/data.ts
@@ -1,11 +1,22 @@
 import { colors } from "./colors";
 
-export interface Playlist {
+export interface album {
     id: number;
-    albumid: number;
+    albumId: number;
     title: string;
     cover: string;
-    song_url: string;
-    artist: string[];
+    artists: string[];
     color: (typeof colors)[keyof typeof colors]
 }
+
+export const albums: album[] = [
+     {
+        id: 1,
+        albumId: 1,
+        title: "Torii Yama",
+        cover: "https://example.com/covers/torii_yama.png",
+        artists: ["Dellafuente"],
+        color: colors.orange,
+     },
+     
+]
