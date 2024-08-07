d9e55cacb30ae5446c5431cee6f3ed98b0472849
jaironf <jaironf.94@gmail.com>
2024-08-07 18:03:29 +0200

    Albumes y colores integrados

--- src/library/colors.ts
+++ src/library/colors.ts
@@ -2,4 +2,8 @@ export const colors = {
 orange: {accent: "#ffa700", dark: "#e39e19"},
 purple: {accent: "#c70376", dark: "#950258"},
 brown: {accent: "#6B2D1F", dark: "#1F0D0A"},
+blue: {accent: "#2F627F", dark: "#162834"},
+ligth_blue: {accent: "#0E7A9D", dark: "#0D303D"},
+green: {accent: "#7D8B6F", dark: "#373D31"},
+ligth_brown: {accent: "#B59F79", dark: "#473F30"},
 }
--- src/library/data.ts
+++ src/library/data.ts
@@ -26,5 +26,68 @@ export const albums: album[] = [
         artists: ["Ébano"],
         color: colors.purple,
      },
-     
+     {
+        id: 3,
+        albumId: 3,
+        title: "Blindao",
+        cover: "https://example.com/covers/blindao.png",
+        artists: ["Oddliquor"],
+        color: colors.brown,
+     },
+     {
+      id: 4,
+      albumId: 4,
+      title: "Tercer verano del amor",
+      cover: "https://example.com/covers/tercer_verano_del_amor.png",
+      artists: ["Nico Miseria"],
+      color: colors.blue,
+     },
+     {
+      id: 5,
+    albumId: 5,
+    title: "Me muevo con Dios",
+    cover:  "https://example.com/covers/me_muevo_con_dios.png",
+    artists: ["Cruz Cafuné"],
+    color: colors.ligth_blue
+     },
+     {
+      id: 6,
+    albumId: 6,
+    title: "La espalda del sol",
+    cover: "https://example.com/covers/la_espalda_del_sol.png",
+    artists: ["Diego 900"],
+    color: colors.green
+     },
+     {
+      id: 7,
+    albumId: 7,
+    title: "Vultures 2",
+    cover: "https://example.com/covers/vultures_2.png",
+    artists: ["Kanye West, Ty Dolla $ign"],
+    color: colors.ligth_brown
+     },
+     {
+      id: 8,
+    albumId: 8,
+    title: "Good kid, m.A.A.d city(Delux)",
+    cover: "https://example.com/covers/Good_kid,_m.A.A.d_city_(Delux).png",
+    artists: ["Kendrick Lamar"],
+    color: colors.ligth_blue
+     },
+     {
+      id: 9,
+    albumId: 9,
+    title: "blu€s",
+    cover: "https://example.com/covers/blu€s.png",
+    artists: ["Cruz Cafuné"],
+    color: colors.orange
+     },
+     {
+      id: 10,
+    albumId: 10,
+    title: "La fe que me tengas",
+    cover: "https://example.com/covers/la_fe_que_me_tengas.png",
+    artists: ["Mvrk"],
+    color: colors.ligth_blue
+     },
 ]
